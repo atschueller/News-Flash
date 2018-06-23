cad7cf929381071fa55c298aa003843917d75f9b
Adrienne Schueller <atschueller@gmail.com>
2018-06-23 14:15:37 -0400

    axios

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cheerio = require("cheerio");
+var axios = require("axios");
 const request = require("request");
-const exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 const db = require('./models');
 
@@ -16,6 +13,8 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+require('./routes/apiroutes')(app);
 
 mongoose.connect('mongodb://localhost/mongoose')
 
